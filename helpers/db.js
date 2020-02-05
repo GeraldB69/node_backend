e7ce08cdea13421ba387ce7d0657722b0f5350e6
Gérald <48840753+GeraldB69@users.noreply.github.com>
2020-02-05 16:13:08 +0100

    Create db.js

---
+++ helpers/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password :  null,
+  database : 'test_hpi'
+});
+
+// Nom de domaine à compléter
+// const httpsPath = "/etc/letsencrypt/live/";
+
+module.exports.connection = connection;
+module.exports.httpsPath = httpsPath;
